715481c0e8f3b86ff428c893e10eb1669b6573d1
Moril <jeanmo333@gmail.com>
2023-01-05 14:41:58 -0300

    commit

--- helpers/email.js
+++ helpers/email.js
@@ -6,6 +6,7 @@ export const emailRegistro = async (datos) => {
   const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
